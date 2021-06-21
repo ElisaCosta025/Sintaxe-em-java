db6832c651802d8210fd02da03623add1ea3bf27
ElisaCosta025 <63672767+ElisaCosta025@users.noreply.github.com>
2021-06-20 21:42:21 -0300

    Create MultTres.java

---
+++ MultTres.java
@@ -0,0 +1,11 @@
+// EX4 da lista - Elisa Costa
+
+class MultTres {
+  public static void main(String[] args){
+    for (int m = 1; m < 100; m++){
+      if (m % 3 == 0){
+        System.out.println(m);
+      }
+    }
+  }
+}
