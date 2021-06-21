218616d303e7fa7acc7c753ef9422e6210d4a911
ElisaCosta025 <63672767+ElisaCosta025@users.noreply.github.com>
2021-06-20 21:44:49 -0300

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+class Fibonacci{
+  public static void main(String[] args){
+    int a = 0;
+    int b = 1;
+    System.out.println(a);
+    System.out.println(b);
+    int c = a + b;
+    while (c < 100) {
+      System.out.println(c);
+      a = b;
+      b = c;
+      c = a + b;
+    }
+  }
+}
