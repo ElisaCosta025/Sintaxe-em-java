9dfb1629113f34aa360ae99c29cbf068a8035a89
ElisaCosta025 <63672767+ElisaCosta025@users.noreply.github.com>
2021-06-20 21:41:22 -0300

    Create Nine.java

---
+++ Nine.java
@@ -0,0 +1,11 @@
+// EXERCÍCIO 3 DA LISTA - ELISA COSTA
+
+class Nine{
+  public static void main(String[] args){
+    int Sum = 0;
+    for (int m = 1; m < 1000; m++){
+      Sum += m;
+    }
+    System.out.println(Sum);
+  }
+}
